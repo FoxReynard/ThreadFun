5466b623f248d4c44ec4c236729e6425ffdc1f55
Dmitry Kravchenko <massacre105@gmail.com>
2020-09-18 01:35:33 +0300

    Add project files.

---
+++ ThreadFun/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Drawing;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace ThreadFun
+{
+	class Program
+	{
+
+
+		static void Print(object lineNumber)
+		{
+
+			ConsoleColor[] colors = (ConsoleColor[])ConsoleColor.GetValues(typeof(ConsoleColor));
+
+
+			while (true)
+			{
+				foreach (var color in colors)
+				{
+					if (color == ConsoleColor.Black)
+						continue;
+
+					
+					Console.ForegroundColor = color;
+					lock (locker)
+					{
+						Console.SetCursorPosition(0, (int)lineNumber);
+						for (int i = 0; i <=40; i++)
+						{
+
+							Console.Write('|');
+							Thread.Sleep(1); 
+						}
+						Console.Write('|');
+						Console.SetCursorPosition(0, (int)lineNumber);
+					}
+					
+				}
+			}
+		}
+
+		static object locker = new object();
+		static void Main(string[] args)
+		{
+			Console.CursorVisible = false;
+
+
+			for (int i = 0; i < 10; i++)
+			{
+				Thread thread = new Thread(new ParameterizedThreadStart(Print));
+				thread.Start(i);
+
+
+
+
+			}
+
+
+
+
+
+			//Print(5);
+		}
+	}
+}
